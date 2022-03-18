505d9f29278ff0b10489c67c101ed0d9479b1749
Gökhan Alkacir <gokhan@pija.se>
2022-03-18 11:22:58 +0100

    added zod for schema and type creation

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './types';
+export * from './schemas';
 export { default as segment } from './segment';
 export { default as isObject } from './isObject';
 export { default as seedRandom } from './seedRandom';
