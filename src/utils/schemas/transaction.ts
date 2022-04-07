d122304d8f655d08394acdbf0c77a2d48a04eafe
Gökhan Alkacir <alkacirg@gmail.com>
2022-04-07 15:47:19 +0200

    added order ref prop to transaction

--- src/utils/schemas/transaction.ts
+++ src/utils/schemas/transaction.ts
@@ -1,13 +1,13 @@
 import { z } from 'zod';
 
 const TransactionSchema = z.object({
-  id: z.string().optional(),
   amount: z.number(),
   date: z.date().optional(),
   customerId: z.string().optional(),
   accountNumber: z.string(),
   accountName: z.string(),
   transactionType: z.string(),
+  orderRef: z.string(),
 });
 
 export default TransactionSchema;
