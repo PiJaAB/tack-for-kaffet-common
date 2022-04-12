44a813dfad426b17aa120c8a95d73aaffeabb112
Sacquer <gokhan@pija.se>
2022-04-12 13:58:54 +0200

    adjusted schema for order, transaction, bumped version

--- src/utils/schemas/order.ts
+++ src/utils/schemas/order.ts
@@ -3,6 +3,18 @@ import ProductSchema from './product';
 import CustomerSchema from './customer';
 import TransactionSchema from './transaction';
 
+const StatusSchema = z.union([
+  z.literal('pending'),               // Inväntar betalning
+  z.literal('manual-processing'),     // Manuell behandling
+  z.literal('processing'),            // Behandlas
+  z.literal('on-hold'),               // Pausad
+  z.literal('completed'),             // Färdigbehandlad
+  z.literal('cancelled'),             // Avbruten
+  z.literal('refunded'),              // Återbetald
+  z.literal('failed'),                // Misslyckad
+  z.literal('unknown'),               // Okänt fel
+]);
+
 const OrderSchema = z.object({
   id: z.string().optional(),
   cancelled: z.boolean(),
@@ -14,6 +26,16 @@ const OrderSchema = z.object({
   customer: CustomerSchema.optional(),
 
   transactions: z.array(TransactionSchema).optional(),
+
+  orderStatus: StatusSchema,
+  paymentStatus: StatusSchema.optional(),   // optional being temporary(!)
+
+  orderNotes: z.array(z.object({
+    id: z.string(),
+    orderId: z.string(),
+    content: z.string(),
+    username: z.string(),
+  })).optional(),
 
 
 
