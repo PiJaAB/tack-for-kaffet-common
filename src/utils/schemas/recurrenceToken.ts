95c223e69e485a533a4eb1db49a936f41ce22c0c
takedo <alkacirg@gmail.com>
2022-08-24 16:18:15 +0200

    style: linted

--- src/utils/schemas/recurrenceToken.ts
+++ src/utils/schemas/recurrenceToken.ts
@@ -1,7 +1,12 @@
 import { z } from 'zod';
 
 const RecurrenceTokenSchema = z.object({
-  type: z.string(),
+  type: z.union([
+    z.literal('payment'),
+    z.literal('recurrence'),
+    z.literal('transactionOnfile'),
+    z.literal('unscheduled'),
+  ]),
   token: z.string(),
   name: z.string(),
   expiryDate: z.string(),
