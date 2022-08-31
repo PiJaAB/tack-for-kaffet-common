103fe3f6d6200317f08908cc7a80cec752a32125
takedo <alkacirg@gmail.com>
2022-08-31 16:49:51 +0200

    style: add comment that represents order payload from WP backend

--- src/utils/schemas/order.ts
+++ src/utils/schemas/order.ts
@@ -34,3 +34,85 @@ const OrderSchema = z.object({
 });
 
 export default OrderSchema;
+
+/**
+ *
+ * tfk-React=frontend typings for Order
+ *
+ * export type OrderStatuses =
+ *   | 'pending'
+ *   | 'processing'
+ *   | 'manual-processing'
+ *   | 'on-hold'
+ *   | 'completed'
+ *   | 'cancelled'
+ *   | 'refunded'
+ *   | 'failed';
+ *
+ * interface HasPaymentMethod {
+ *   paymentMethod: {
+ *     id: string;
+ *     title: string;
+ *   } | null;
+ * }
+ *
+ * export interface OrderSchema extends HasPaymentMethod {
+ *   id: number;
+ *   total: string;
+ *   completed: number | null;
+ *   paid: number | null;
+ *   status: OrderStatuses;
+ *   humanStatus: string;
+ * }
+ *
+ * {
+    "type": "order",
+    "data": {
+      "id": 314803,
+      "total": "39",
+      "completed": null,
+      "paid": null,
+      "payment_method": {
+        "id": "payex_psp_cc",
+        "title": "Credit Card"
+      },
+      "status": "pending"
+    },
+    "subscription": {
+        "id": 314804,
+        "dates": {
+            "start": "2022-08-31T13:57:47+0000",
+            "next_payment": null,
+            "end": "2022-08-31T13:58:08+0000",
+            "trial_end": null
+        },
+        "status": "cancelled",
+        "price": "39",
+        "period": "month",
+        "interval": 1,
+        "payment_method": {
+            "id": "payex_psp_cc",
+            "title": "Credit Card"
+        },
+        "needs_payment": false,
+        "auto_renewal": false,
+        "can_cancel": false,
+        "can_reactivate": false,
+        "can_resubscribe": true,
+        "can_change_payment_method": false,
+        "can_add_payment": false,
+        "last_payment": {
+            "id": 314803,
+            "total": "39",
+            "completed": null,
+            "paid": null,
+            "payment_method": {
+                "id": "payex_psp_cc",
+                "title": "Credit Card"
+            },
+            "status": "pending",
+            "needs_payment": true
+        }
+    }
+}
+ * */
