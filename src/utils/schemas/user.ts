2b05d2a824b0b223526fd066f0c2e4985c6bc329
Gökhan Alkacir <alkacirg@gmail.com>
2022-09-22 16:32:07 +0200

    feat: bumped version, infer user type from schema

--- src/utils/schemas/user.ts
+++ src/utils/schemas/user.ts
@@ -2,9 +2,39 @@ import { z } from 'zod';
 
 const UserSchema = z.object({
   id: z.string().optional(),
+  email: z.string().optional(),
+  emailVerified: z.boolean(),
+  phoneNumber: z.string().nullish(),
+  accessFlags: z.record(z.boolean()).nullable(),
+  tempPassword: z.boolean().optional(),
+  disabled: z.boolean(),
+  deviceTokens: z.array(z.string()).optional(),
+  subscribedTopics: z.array(z.string()).optional(),
+  notificationKey: z.string().nullish(),
   role: z.string().optional(),
   username: z.string().optional(),
+  displayName: z.string().optional(),
   private: z.never().optional(),
+});
+
+export const PublicUserSchema = z.object({
+  username: z.string().optional(),
+  role: z.string().optional(),
+  private: z.never(),
+});
+
+export const PrivateUserSchema = PublicUserSchema.extend({
+  private: z.object({
+    email: z.string().optional(),
+    emailVerified: z.boolean(),
+    phoneNumber: z.string().optional(),
+    accessFlags: z.record(z.boolean()).nullable(),
+    tempPassword: z.boolean().optional(),
+    disabled: z.boolean(),
+    deviceTokens: z.array(z.string()).optional(),
+    subscribedTopics: z.array(z.string()).optional(),
+    notificationKey: z.string().nullish(),
+  }),
 });
 
 export default UserSchema;
--- src/utils/types/user.ts
+++ src/utils/types/user.ts
@@ -1,25 +1,10 @@
 import { z } from 'zod';
 
-import UserSchema from '../schemas/user';
+import UserSchema, {
+  PublicUserSchema,
+  PrivateUserSchema,
+} from '../schemas/user';
 
 export type User = z.infer<typeof UserSchema>;
-
-export interface PublicUser {
-  username?: string;
-  role?: string;
-  private?: never;
-}
-
-export interface PrivateUser extends Omit<PublicUser, 'private'> {
-  private: {
-    email?: string;
-    emailVerified: boolean;
-    phoneNumber?: string;
-    accessFlags?: Partial<Record<string, boolean>> | null;
-    tempPassword?: boolean;
-    disabled: boolean;
-    deviceTokens?: string[];
-    subscribedTopics?: string[];
-    notificationKey?: string | null;
-  };
-}
+export type PublicUser = z.infer<typeof PublicUserSchema>;
+export type PrivateUser = z.infer<typeof PrivateUserSchema>;
