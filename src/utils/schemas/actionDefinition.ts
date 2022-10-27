51249835d0744379b55ec449a69074849b5664c9
Linn Dahlgren <linn.dahlgren@pija.se>
2022-10-27 18:20:33 +0200

    Udpated typings

--- src/utils/schemas/actionDefinition.ts
+++ src/utils/schemas/actionDefinition.ts
@@ -3,13 +3,13 @@ import { z } from 'zod';
 const ActionDefinitionSchema = z.object({
   access: z.array(z.union([z.literal('admin'), z.literal('user')])),
   actionName: z.string(),
-  params: z.unknown().optional(),
+  params: z.any().optional(),
 });
 
 export const PerformedActionSchema = z.object({
   actionName: z.string(),
   paymentMethod: z.string(),
-  params: z.unknown().optional(),
+  params: z.any().optional(),
 });
 
 export default ActionDefinitionSchema;
